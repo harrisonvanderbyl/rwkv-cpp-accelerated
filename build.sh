28af50fbc049481dd7c9801d32dcb59deb1c945e
Harrison <harrison.vanderbyl@gmail.com>
2023-04-23 22:21:02 +1000

    slightly better

--- build.sh
+++ build.sh
@@ -1,16 +1,19 @@
-# if linux
-if $OSTYPE == "linux-gnu"; then
-    echo "compiling for linux"
-    nvcc ./rwkv.cu -c
-    g++ ./rwkv.o ./chat.cpp -lcudart_static -L/usr/local/cuda/lib64
-    rm ./rwkv.o
-    mv ./a.out ./release/chat
-else
-    echo "compiling for windows"
-    mkdir build
-    cd build
-    cmake ..
-    cmake --build . --config release
-    cd ..
-    mv ./build/release/chat.exe ./release/chat.exe
-fi
+# simplistic build script equivalent to the following commands
+# nvcc ./rwkv.cu -c
+# g++ ./rwkv.o ./chat.cpp -lcudart_static -L/usr/local/cuda/lib64
+# rm ./rwkv.o
+# mv ./a.out ./release/chat
+
+
+echo "compiling"
+mkdir build
+cd build
+cmake ..
+cmake --build . --config release
+cd ..
+# windows
+mv ./build/release/rwkv.exe ./release/chat.exe
+# linux
+mv ./build/release/rwkv ./release/chat
+
+
