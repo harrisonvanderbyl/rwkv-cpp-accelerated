7f5badb34e8d51b6d8c47bf7fff3dc129d929d99
Harrison <harrison.vanderbyl@gmail.com>
2023-04-25 12:05:30 +1000

    change location

--- examples/terminalchat/build.bat
+++ examples/terminalchat/build.bat
@@ -4,5 +4,5 @@ cd build
 cmake ..
 cmake --build . --config release
 cd ..
-copy .\build\rwkv.exe .\release\chat.exe
+copy .\build\Release\rwkv.exe .\release\chat.exe
 copy ..\..\include\rwkv\tokenizer\vocab .\release\vocab /s /e /y
