362f8001584066928449a8b3722dcc3b3b323a6b
Chernenko Ruslan <ractyfree@gmail.com>
2023-04-27 16:15:56 +0300

    Create initial fully gone

--- bindings/pybind/c_binding.cpp
+++ bindings/pybind/c_binding.cpp
@@ -6,98 +6,74 @@
 
 namespace py = pybind11;
 
-RWKV Rwkv = RWKV();
 
-py::list initState(){
-
-    py::array_t<double> statexy_array = py::array_t<double>(Rwkv.num_layers * Rwkv.num_embed);
-    py::buffer_info statexy_buf = statexy_array.request();
-
-    py::array_t<double> stateaa_array = py::array_t<double>(Rwkv.num_layers * Rwkv.num_embed);
-    py::buffer_info stateaa_buf = stateaa_array.request();
-
-    py::array_t<double> statebb_array = py::array_t<double>(Rwkv.num_layers * Rwkv.num_embed);
-    py::buffer_info statebb_buf = statebb_array.request();
-
-    py::array_t<double> statepp_array = py::array_t<double>(Rwkv.num_layers * Rwkv.num_embed);
-    py::buffer_info statepp_buf = statepp_array.request();
-
-    py::array_t<double> statedd_array = py::array_t<double>(Rwkv.num_layers * Rwkv.num_embed);
-    py::buffer_info statedd_buf = statedd_array.request();
-
-    Rwkv.statexy  = (double*)statexy_buf.ptr;
-    Rwkv.stateaa  = (double*)stateaa_buf.ptr;
-    Rwkv.statebb  = (double*)statebb_buf.ptr;
-    Rwkv.statepp  = (double*)statepp_buf.ptr;
-    Rwkv.statedd  = (double*)statedd_buf.ptr;
-
-    for (unsigned long long i = 0; i < Rwkv.num_layers*Rwkv.num_embed; i++) {
-        Rwkv.statexy[i] = 0;
-        Rwkv.stateaa[i] = 0;
-        Rwkv.statebb[i] = 0;
-        Rwkv.statepp[i] = 0;
-        Rwkv.statedd[i] = 0;
-    }
-
-    py::list return_list;
-    return_list.append(statexy_array);
-    return_list.append(stateaa_array);
-    return_list.append(statebb_array);
-    return_list.append(statepp_array);
-    return_list.append(statedd_array);
-    return return_list;
-}
-
-py::list getRwkvState() {
-    py::list return_list;
-
-    return_list.append(Rwkv.statexy);
-    return_list.append(Rwkv.stateaa);
-    return_list.append(Rwkv.statebb);
-    return_list.append(Rwkv.statepp);
-    return_list.append(Rwkv.statedd);
-
-    return return_list;
+RWKV* initRwkv() {
+    return &RWKV();
 
 }
 
-py::array_t<float> initOutput() {
+void initRwkvState(RWKV &rwkv){
+    return return_list;
+}
+
+
+void initRwkvOutput(RWKV &rwkv) {
     py::array_t<float> out_array = py::array_t<float>(50277);
     py::buffer_info out_buf = out_array.request();
 
-    Rwkv.out  = (float*)out_buf.ptr;
+    rwkv.out  = (float*)out_buf.ptr;
 
     for (unsigned long long i = 0; i < 50277; i++) {
-	    Rwkv.out[i] = 0;
+	    rwkv.out[i] = 0;
 	}
 
     return out_array;
 }
 
-float* getRwkvOutput() {
-	return Rwkv.out;
+
+py::array_t<float> getRwkvOutput(RWKV &rwkv) {
+    py::array_t<float> out_array = py::array_t<float>(50277);
+    py::buffer_info out_buf = out_array.request();
+
+    for (unsigned long long i = 0; i < 50277; i++) {
+        out_buf.ptr[i] = rwkv.out[i];
+    }
+
+    return out_array;
 
 }
 
-std::tuple<int64_t, int64_t> loadWrapper(const std::string& filename){
-    Rwkv.loadFile(filename);
+py::list getRwkvState(RWKV &rwkv) {
 
+
+
+}
+
+std::tuple<int64_t, int64_t> loadWrapper(RWKV &rwkv, const std::string& filename){
+    rwkv.loadFile(filename);
     return std::make_tuple(Rwkv.num_layers, Rwkv.num_embed);
 }
 
-void cuda_rwkv_wrapper(int64_t token){
+void modelForward(RWKV &rwkv, int64_t token){
     Rwkv.forward(token);
 }
 
 
 PYBIND11_MODULE(rwkv, m) {
-    m.def("rwkvc", &cuda_rwkv_wrapper, "rwkvc");
-    m.def("load", &loadWrapper, "load");
-    m.def("initState", &initState, "initState");
-    m.def("initOutput", &initOutput, "initOutput");
+    m.def("modelForward", &cuda_rwkv_wrapper, "rwkvc");
+    m.def("loadModel", &loadWrapper, "load");
 
-    m.def("getRwkvOutput", &getRwkvOutput, "getRwkvOutput");
+    m.def("initState", &initState, "initState");
     m.def("getRwkvState", &getRwkvState, "getRwkvState");
+
+    m.def("initOutput", &initOutput, "initOutput");
+    m.def("getRwkvOutput", &getRwkvOutput, "getRwkvOutput");
+
+    m.def("loadTokenizer", &getRwkvOutput, "getRwkvOutput");
+    m.def("tokenizerEncode", &getRwkvOutput, "getRwkvOutput");
+    m.def("tokenizerDecode", &getRwkvOutput, "getRwkvOutput");
+
+    m.def("typicalSample", &getRwkvOutput, "getRwkvOutput");
 
 }
 
