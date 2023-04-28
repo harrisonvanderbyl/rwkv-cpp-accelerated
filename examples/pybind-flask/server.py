f87e4e92e0ce945f55fce1cc8f021eb146deec56
Chernenko Ruslan <ractyfree@gmail.com>
2023-04-28 13:15:15 +0300

    Add generate tokens logic

--- examples/pybind-flask/server.py
+++ examples/pybind-flask/server.py
@@ -1,5 +1,7 @@
 import os
+import json
 
+from typing import List
 from dataclasses import dataclass
 
 from flask import Flask
@@ -20,16 +22,61 @@ TOKENIZER = binding.TokenizerWrapper(
         merges_path=MERGES_PATH
         )
 
+
 @dataclass
-class CompleteDataclass:
+class CompleteRequestDataclass:
     body: str
     tokens: int
+    stop_sequence: str = None
+    with_body = False
 
+
+def generate_tokens(
+    *, 
+    model: binding.ModelWrapper,
+    input_tokens: List[int],
+    tokens_to_generate: int,
+    ):
+    
+    model.init_state()
+    model.load_context(input_tokens)
+    last_token = input_tokens[-1]
+    for _ in range(tokens_to_generate):
+        model.forward(last_token)
+        last_token = model.sample()
+        yield last_token
+
+
+def complete_string(complete: CompleteRequestDataclass):
+    input_tokens = TOKENIZER.encode(complete.body)
+    output_tokens = generate_tokens(
+            model=MODEL, 
+            input_tokens=input_tokens,
+            tokens_to_generate=complete.tokens
+            )
+    stop_sequence_window = complete.stop_sequence * -1
+
+    return_string = complete.body if complete.with_body else ""
+    for output_token in output_tokens:
+        decoded_token = TOKENIZER.decode(output_token)
+        return_string += decoded_token
+        print(decoded_token, end='')
+        
+        # if stop sequence occured(a substring of chat history containing reverse sequence)
+        # then we need to reprompt here
+
+        if return_string[stop_sequence_window:] == complete.stop_sequence:
+            # need to reprompt here
+            break
+
+    return return_string
+ 
+ 
 @app.route('/complete', methods=['POST'])
 def add_message():
-    complete = CompleteDataclass(**request.json)
-    print(complete)
-    return "received"
+    complete = CompleteRequestDataclass(**request.json)
+    completed = complete_string(complete)
+    return json.dumps({"completion": completed})
 
 
 if __name__ == "__main__":
